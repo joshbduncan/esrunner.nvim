44d056901258974e29ac605d970be8a048fe481d
Josh Duncan <44387852+joshbduncan@users.noreply.github.com>
2025-01-08 12:34:47 -0500

    testing, formatting, pre-commit

---
+++ Makefile
@@ -0,0 +1,16 @@
+# Run all test files
+test: deps/mini.nvim
+	nvim --headless --noplugin -u ./scripts/minimal_init.lua -c "lua MiniTest.run()"
+
+# Run test from file specified with FILE=path/to/file
+test_file: deps/mini.nvim
+	nvim --headless --noplugin -u ./scripts/minimal_init.lua -c "lua MiniTest.run_file('$(FILE)')"
+
+# Download 'mini.nvim' to use its 'mini.test' testing module
+deps/mini.nvim:
+	@mkdir -p deps
+	git clone --filter=blob:none https://github.com/echasnovski/mini.nvim $@
+
+# Run StyLua
+format:
+	stylua lua/ plugin/
--- scripts/minimal_init.lua
+++ scripts/minimal_init.lua
@@ -1,13 +1,12 @@
--- Add project root as full path to runtime path (in order to be able to
--- `require()`) modules from this module
+-- Add current directory to 'runtimepath' to be able to use 'lua' files
 vim.cmd([[let &rtp.=','.getcwd()]])
 
--- Ensure persistent color scheme (matters after new default in Neovim 0.10)
-vim.o.background = "dark"
-require("mini.hues").setup({ background = "#11262d", foreground = "#c0c8cc" })
+-- Set up 'mini.test' only when calling headless Neovim (like with `make test`)
+if #vim.api.nvim_list_uis() == 0 then
+	-- Add 'mini.nvim' to 'runtimepath' to be able to use 'mini.test'
+	-- Assumed that 'mini.nvim' is stored in 'deps/mini.nvim'
+	vim.cmd("set rtp+=deps/mini.nvim")
 
--- - Make screenshot tests more robust across Neovim versions
-if vim.fn.has("nvim-0.11") == 1 then
-	vim.api.nvim_set_hl(0, "PmenuMatch", { link = "Pmenu" })
-	vim.api.nvim_set_hl(0, "PmenuMatchSel", { link = "PmenuSel" })
+	-- Set up 'mini.test'
+	require("mini.test").setup()
 end
